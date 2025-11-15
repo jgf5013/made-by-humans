a0fc34c525808c3ea02e9444887a3450f5d70767
John Fisher <jgf5013@gmail.com>
2025-11-15 18:20:44 +0100

    chore: basic svelte app setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Human Made</h1>
+<p>We're a foundation based in the Netherlands. We want to ensure customers know whether or not products were made with or without AI</p>
+
+<p>Contact <span>jgf5013@gmail.com</span> if your interested in using our label</p>
+
+<p>Note: Formation of the Foundation is in progress - November 2025</p>
