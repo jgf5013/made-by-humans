61ffe1d6e9d9bc7c6f29db7bec7ceb25ed444350
John Fisher <jgf5013@gmail.com>
2026-03-01 20:28:30 +0100

    feat(ui): using skeleton design system

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,48 @@
+<script lang="ts">
+	import Counter from './Counter.svelte';
+	import layoutCssUrl from './layout.css?url';
+</script>
+
+<svelte:head>
+	<title>Made By Humans</title>
+	<meta name="description" content="Provides verification and labeling services for artists and creators" />
+	<link rel="stylesheet" href={layoutCssUrl} />
+</svelte:head>
+
 <main class="app">
-  <h1 class="app-header typography">
-    HUMAN MADE
-  </h1>
-  <div>
-    <wa-heading-l>Human Made</wa-heading-l>
-    <p>We're a foundation based in the Netherlands. We want to ensure customers know whether or not products were made with or without AI</p>
+	<h1 class="h1">Made By Humans</h1>
 
-    <p>Contact <span>jgf5013@gmail.com</span> if your interested in using our label</p>
+	<section>
+		<h2 class="h2">
+			Our Mission
+		</h2>
+		<p>To ensure consumers of physical and digital media are aware of whether or not Generative AI was used in the production of some product. The organization will provide a certification service and resulting trademarked label for this purpose. The organization intends for this label to be used internationally for things like brochures, websites, designs on t-shirts and hats, books, movies, etc.</p>
+	</section>
 
-    <p>Note: Formation of the Foundation is in progress - November 2025</p>
-  </div>
+	<section>
+		<h2 class="h2">
+			Our Vision
+		</h2>
+		<p>We believe in a world where creators are recognized and rewarded for their originality, and where consumers can make informed choices about the products use. Our vision is to establish a trusted standard for identifying and labeling content created with Generative AI, fostering transparency and accountability in the creative industry.</p>
+	</section>
 </main>
 
 <style>
 
   .app {
-      text-align: center;
-      height: 100%;
+		text-align: center;
+		height: 100%;
   }
 
   .app-header {
-      height: 10%;
+		height: 10%;
   }
+
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
 </style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+	import { page } from '$app/state';
+	import logo from '$lib/images/fingerprint.svg';
+</script>
+
+<header>
+	<div class="corner">
+		<a href="https://svelte.dev/docs/kit">
+			<img id="logo" src={logo} alt="Made By Humans Logo" />
+		</a>
+	</div>
+
+	<nav>
+	</nav>
+</header>
+
+<style>
+	#logo {
+		height: 5rem;
+	}
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+</style>
